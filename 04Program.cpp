f894cd11a30a3de07577e012a5c4d886f4e5959f
gagan <gaganag50@gmail.com>
2018-09-12 09:27:04 +0530

    05 file created

---
+++ 04Program.cpp
@@ -0,0 +1,10 @@
+Write a C++ program to create a simple banking system in which the initial balance and the rate of interest 
+are read from the keyboard and these values are initialized using the constructor. The destructor member 
+function is defined in this progrm to destroy the class object created using constructor member function. This prgrma consists of following member functions:
+1--> Constructor to initialize the balance and rate of interest
+2--> Deposit - To make deposit
+3--> Withdraw - To with draw an amount
+4--> Compound - To find compound interest
+5--> getBalance - To know the balance amount
+6--> Menu - To display menu options
+7--> Destructor
